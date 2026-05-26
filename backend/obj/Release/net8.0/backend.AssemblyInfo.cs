2eeb0f9c359aaa73356abca4b068eb45d46bac4d
Eduardo Moraes <eduardobaldom2@gmail.com>
2026-05-26 20:32:33 -0300

    Adiciona gitignore

--- backend/obj/Release/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Release/net8.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b91710e13dbc24897b83f65d60460dda1d169110")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
