088da3a8505d567103a88e5b20a0ef82c63b9427
Eduardo Moraes <eduardobaldom2@gmail.com>
2026-05-27 19:44:44 -0300

    CRUD completo de pesquisadores

--- backend/Controllers/PesquisadoresController.cs
+++ backend/Controllers/PesquisadoresController.cs
@@ -22,4 +22,51 @@ public class PesquisadoresController : ControllerBase
 
         return Ok(pesquisadores);
     }
+
+    [HttpPost]
+    public IActionResult Post(Pesquisador pesquisador)
+    {
+        _context.Pesquisadores.Add(pesquisador);
+
+        _context.SaveChanges();
+
+        return CreatedAtAction(nameof(Get), new { id = pesquisador.Id }, pesquisador);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Put(int id, Pesquisador pesquisadorAtualizado)
+    {
+        var pesquisador = _context.Pesquisadores.Find(id);
+
+        if (pesquisador == null)
+        {
+            return NotFound();
+        }
+
+        pesquisador.Nome = pesquisadorAtualizado.Nome;
+        pesquisador.Email = pesquisadorAtualizado.Email;
+        pesquisador.Curso = pesquisadorAtualizado.Curso;
+        pesquisador.Departamento = pesquisadorAtualizado.Departamento;
+
+        _context.SaveChanges();
+
+        return Ok(pesquisador);
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var pesquisador = _context.Pesquisadores.Find(id);
+
+        if (pesquisador == null)
+        {
+            return NotFound();
+        }
+
+        _context.Pesquisadores.Remove(pesquisador);
+
+        _context.SaveChanges();
+
+        return NoContent();
+    }
 }
